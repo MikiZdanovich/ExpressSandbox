65343740906c2aef98b23e3d9d91a8fc2310369c
m-zdanovich <nikzdanovich@gmail.com>
2022-02-12 15:30:05 +0300

    JWT user create login

---
+++ src/controllers/UserController.js
@@ -0,0 +1,9 @@
+const Controller = require('../controllers/Controller')
+const service = require('../service/UserService')
+const createUser = async (request, response) => {
+  await Controller.handleRequest(request, response, service.createUser)
+}
+
+module.exports = {
+  createUser
+}
---
+++ src/service/UserService.js
@@ -0,0 +1,25 @@
+const logger = require('../../logger')
+const Service = require('./Service')
+const { setUserParams } = require('../utils/userUtils')
+
+const models = require('../db/models')
+
+async function createUser (request) {
+  try {
+    const payload = setUserParams(request)
+    const user = await models.User.create(
+      payload
+    )
+    logger.info(user)
+    return Service.successResponse(null, 200)
+  } catch (e) {
+    return Service.rejectResponse(
+      e.message || 'Invalid input',
+      e.status || 405
+    )
+  }
+}
+
+module.exports = {
+  createUser
+}
