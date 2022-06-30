5428f2bf742c971c17f59831ba14ccc620dfca44
HannaKavalionak <Hanna0Kavalionak@gmail.com>
2022-06-30 18:16:41 +0300

    Add create and get category

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,12 +1,13 @@
 const Controller = require('./Controller');
-const CategoryService = require('../service/CategoryService.js')
+const CategoryService = require('../service/CategoryService.js');
+const category = require('../db/models/category');
 
 const addCategory = async (request, response) => {
     requestBody = request.body
     categoryName = requestBody["name"]
     categoryDescription = requestBody["description"]
     userId = request.user["id"]
-    category = await CategoryService.createCategory(categoryName, categoryDescription, userId)
+    const category = await CategoryService.createCategory(categoryName, categoryDescription, userId)
 
     Controller.sendResponse(response, Controller.successResponse(category, 201))
 }
@@ -16,7 +17,7 @@ const getCategory = async (request, response) => {
     categoryId = request.params["categoryId"]
 
 
-    category = await CategoryService.getCategory(categoryId)
+    const category = await CategoryService.getCategory(categoryId)
 
     if (!category) {
         Controller.sendResponse(response, Controller.successResponse("Not Found", 404))
